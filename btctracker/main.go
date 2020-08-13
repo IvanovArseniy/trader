c280b8605e9ebc0abf23dd1b30bd6fa3ccf0a23d
Arseniy Ivanov <arivanov@gmcs.ru>
2020-08-13 11:56:51 +0300

    Feat: Add loop to save ticket data each five seconds

--- btctracker/main.go
+++ btctracker/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 	binanceservice "trader/btctracker/subpackages/binanceservice"
 	postgresservice "trader/btctracker/subpackages/postgresservice"
 )
 
 func main() {
 	fmt.Printf("Trader started\n")
-	ticket := binanceservice.GetTicket()
-	postgresservice.SaveTicket(ticket)
-	fmt.Printf("ID: %v, Bid: %v, Ask: %v, CreatedOn: %v", ticket.ID, ticket.Bid, ticket.Ask, ticket.CreatedOn)
+	for {
+		ticket := binanceservice.GetTicket()
+		postgresservice.SaveTicket(ticket)
+		fmt.Printf("ID: %v, Bid: %v, Ask: %v, CreatedOn: %v\n", ticket.ID, ticket.Bid, ticket.Ask, ticket.CreatedOn)
+		time.Sleep(5 * time.Second)
+	}
 }
