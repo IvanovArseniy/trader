1a6fd295f121032382f87ca837814e24e3ed2846
Arseniy Ivanov <arivanov@gmcs.ru>
2020-08-24 23:45:12 +0300

    Feat: add gorutines

--- btctracker/main.go
+++ btctracker/main.go
@@ -3,37 +3,48 @@ package main
 import (
 	"fmt"
 	"time"
+	tracker "trader/btctracker/root"
 	binanceservice "trader/btctracker/subpackages/binanceservice"
 	postgresservice "trader/btctracker/subpackages/postgresservice"
 )
 
 func main() {
 	fmt.Printf("Tracker started\n")
+	ticketChannel := make(chan tracker.Ticket)
+
 	i := 0
 	for {
 		i++
 		time.Sleep(5 * time.Second)
-		ticket, err := binanceservice.GetTicket()
-		if err != nil {
-			fmt.Printf("Error occured %v\n", err)
-			continue
-		}
-		_, err = postgresservice.SaveTicket(ticket)
-		if err != nil {
-			fmt.Printf("Error occured %v\n", err)
-		}
-		if i == 10 {
-			i = 0
-			err = postgresservice.CalculateCandle()
+		go func() {
+			ticket, err := binanceservice.GetTicket()
 			if err != nil {
 				fmt.Printf("Error occured %v\n", err)
 			}
-			err = postgresservice.CalculateLevel()
-			if err != nil {
-				fmt.Printf("Error occured %v\n", err)
-			}
-		}
+			fmt.Printf("ID: %v, Bid: %v, Ask: %v, CreatedOn: %v\n", ticket.ID, ticket.Bid, ticket.Ask, ticket.CreatedOn)
+			ticketChannel <- ticket
+		}()
 
-		fmt.Printf("ID: %v, Bid: %v, Ask: %v, CreatedOn: %v\n", ticket.ID, ticket.Bid, ticket.Ask, ticket.CreatedOn)
+		go func() {
+			ticket := <-ticketChannel
+			_, err := postgresservice.SaveTicket(ticket)
+			if err != nil {
+				fmt.Printf("Error occured %v\n", err)
+			}
+		}()
+
+		if i == 12 {
+			i = 0
+			go func() {
+				err := postgresservice.CalculateCandle()
+				if err != nil {
+					fmt.Printf("Error occured %v\n", err)
+				}
+				err = postgresservice.CalculateLevel()
+				if err != nil {
+					fmt.Printf("Error occured %v\n", err)
+				}
+			}()
+		}
 	}
 }
