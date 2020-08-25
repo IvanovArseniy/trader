ff2efd9135c1c55ca51a12e3c71698b79d6c1acd
Arseniy Ivanov <arivanov@gmcs.ru>
2020-08-25 11:06:38 +0300

    Chore: move gap from 50 to 30 for order creation

--- btcorderer/subpackages/postgresservice/postgresservice.go
+++ btcorderer/subpackages/postgresservice/postgresservice.go
@@ -27,7 +27,7 @@ func GetLevel(bid float64) (level orderer.Level, err error) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("select \"Id\", \"bidfrom\", \"bidto\" from \"Level\" where (\"bidto\") > $1 and (\"bidfrom\" - 50) < $1 and \"active\" = 1 and \"deleted\" = 0", bid)
+	rows, err := db.Query("select \"Id\", \"bidfrom\", \"bidto\" from \"Level\" where (\"bidto\") > $1 and (\"bidfrom\" - 30) < $1 and \"active\" = 1 and \"deleted\" = 0", bid)
 	if err != nil {
 		return
 	}
